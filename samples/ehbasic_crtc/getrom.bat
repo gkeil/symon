f49778f558247bc9bc7b16489c822d3a430e565d
Guillermo Keil <gkeil@arnet.com.ar>
2014-12-06 18:42:18 -0300

    Revert "First version of by line refresh with all files"
    This reverts commit 75d9e943e9c1c11e108086b49a9fbea3b43dc528.

--- samples/ehbasic_crtc/getrom.bat
+++
@@ -1,5 +0,0 @@
-
-"C:\Users\Guille\Dropbox\Java\6502 Simulator Resources\cc65\bin\ca65" -o ehbasic.o min_mon.asm 
-
-"C:\Users\Guille\Dropbox\Java\6502 Simulator Resources\cc65\bin\ld65" -C symon.config -vm -m ehbasic.map -o ehbasic.rom ehbasic.o 
-
