bd7ac09c5a54974a08da0e6dfc7a1dcf2d251689
Guillermo Keil <gkeil@arnet.com.ar>
2015-03-09 16:36:14 -0300

    Developing new approach of Time tracking tool
    Developing new approach of Time tracking tool
    
    Signed-off-by: Guillermo Keil <gkeil@arnet.com.ar>

--- src/main/java/com/loomcom/symon/ui/TimeTrack.java
+++ src/main/java/com/loomcom/symon/ui/TimeTrack.java
@@ -1,24 +1,77 @@
 package com.loomcom.symon.ui;
 
+/**
+ * This class is intended to provide Time tracking tools.
+ * All the members and methods provided are static so that
+ * they can used without the need of creating TimeTracking objects.
+ * Time line is compossed of nanoTime stored in a array of longs
+ * @author GKEIL
+ *
+ */
 public class TimeTrack {
 
-	static final int MAX_TOPS = 10;
-	static long st[] = new long[MAX_TOPS];
+	/*
+	 **************************
+	 * TimeTrack class members
+	 **************************/
+	
+	private static final int MAX_TOPS = 100;			// max independent time lines
+	private static long st[] = new long[MAX_TOPS];		// array of longs that holds the
+	private static String stDesc[] = new String[MAX_TOPS];	// String array to store a description of the moment
+	private static int event = 0;						// position in array to to set next time 
 	
 	
-	static void startTime(int top) {
+	/*
+	 * **************************
+	 * Timetracking provide methods
+	 *******************************/
+	
+	
+	/**
+	 * resetTimeline()
+	 * This methods reset TimeTrack variables to start a new time line
+	 */
+	public static void resetTimeLine() {
 		
-		if ( top < MAX_TOPS )
-			st[top] = System.nanoTime();
-	}
+		event = 0;							// reset event number
 	
-	static void showDeltaTime( int top ) {
+		for ( int i=0; i < MAX_TOPS; i++) {	// clear tops array
+			st[i] = 0;
+			stDesc[i] = "";
 		
-		if ( top < MAX_TOPS )
-			System.out.println( "Delta " + top + "is " + (System.nanoTime() - st[top]));
+		}	// end for
 		
+	}	// end resetTimeLine
+	
+	/**
+	 * top()
+	 * This methods stores a new time value on the next available position in the array
+	 * Time table is constructed by storing succesive time values
+	 */
+	public static void top( String moment) {
+	
+		if ( event < MAX_TOPS ) {
+			st[event++] = System.nanoTime();		// store a time value and point to next position
+			stDesc[event++] = moment;				// store corresponding moment description
+		}
 		
-	}
+	}	// end top
+	
+
+	/**
+	 * getTiemLineLength()
+	 * Returns the number of time values stored in the time line
+	 */
+	
+	public static int getTimeLineLength() {
+		
+		return event;		// the are events time values stored
+		
+	}	// end getTimeLineLength
 	
 	
-}
+	
+	
+	
+		
+}	// end TimeTracking class
